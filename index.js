872cc6e2788bf7e094dcf392aa19ff44cdc0ee52
jm1224k <jm1224k@gmail.com>
2020-02-23 19:41:19 +0900

    third commit maybe

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-alert("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "mimi", age = 26, gender = "female";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-alert("hello")
+const name = "mimi",
+  age = 26,
+  gender = "female"
+
+const sayHi = (name, age, gender?) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`)
+}
+
+sayHi(name, age)
+
+export {}
