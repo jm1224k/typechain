c0ae42b90ea41e4f31d620f62cbe1d674b80e9ca
jm1224k <jm1224k@gmail.com>
2020-02-23 20:23:15 +0900

    sixth commit

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const name = "mimi", age = 26, gender = "female";
-const person = {
-    name: "mimi",
-    gender: "female",
-    age: 24
-};
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const kim = new Human("kim", 22, "female");
 const sayHi = (person) => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
-console.log(sayHi(person));
+console.log(sayHi(kim));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,23 @@ const name = "mimi",
   age = 26,
   gender = "female"
 
-interface Human {
-  name: string
-  age: number
-  gender: string
+class Human {
+  public name: string
+  public age: number
+  public gender: string
+  constructor(name: string, age: number, gender: string) {
+    this.name = name
+    this.age = age
+    this.gender = gender
+  }
 }
 
-const person = {
-  name: "mimi",
-  gender: "female",
-  age: 24
-}
+const kim = new Human("kim", 22, "female")
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`
 }
 
-console.log(sayHi(person))
+console.log(sayHi(kim))
 
 export {}
